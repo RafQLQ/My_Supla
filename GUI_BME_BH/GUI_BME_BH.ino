8987a8f8b10f5b38a7d51098eab141d84772130b
Rafal <52934319+RafQLQ@users.noreply.github.com>
2019-12-14 16:29:32 +0100

    Add files via upload

---
+++ GUI_BME_BH/GUI_BME_BH.ino
@@ -0,0 +1,299 @@
+/*
+Copyright (C) AC SOFTWARE SP. Z O.O.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+#include <FS.h>       // ---- esp board manager 2.5.2 --- iwip Variant V2 higher Bandwidth
+#include <SPI.h>
+#include <SuplaDevice.h>
+#include <SuplaImpulseCounter.h>
+#include <io.h>
+#include <WiFiManager.h>
+#include <ArduinoJson.h> //--------- https://github.com/bblanchon/ArduinoJson/tree/v5.13.2 ------
+
+#include <Wire.h>
+
+#include <Adafruit_BME280.h>
+Adafruit_BME280 bme; // I2C
+
+#include <BH1750FVI.h>
+BH1750FVI LightSensor(BH1750FVI::k_DevModeContLowRes);
+
+extern "C"
+{
+#include "user_interface.h"
+} 
+
+//SoftwareSerial pzem;
+//#define D0 16  //no internal pullup resistor
+//#define D1  5
+//#define D2  4
+//#define D3  0  //must not be pulled low during power on/reset, toggles value during boot
+//#define D4  2  //must not be pulled low during power on/reset, toggles value during boot
+//#define D5 14
+//#define D6 12
+//#define D7 13
+//#define D8 15  //must not be pulled high during power on/reset
+
+int wificonfig_pin = 0; // D3
+int C_W_state = HIGH; 
+int last_C_W_state = HIGH;
+unsigned long time_last_C_W_change = 0; 
+long C_W_delay = 5000;               // ---------------------- config delay 5 seconds ---------------------------
+char Supla_server[81];
+char Email[81];
+char Wys[51];
+char Supla_name[51];
+char Supla_status[51];
+byte mac[6];
+bool shouldSaveConfig = false;
+bool initialConfig = false;
+//int customFieldLength = 40;  // ---part of custom Field ( buttons )--
+//WiFiManagerParameter custom_field;  // ---part of custom Field ( buttons )--
+int npm;
+
+/* Use software serial for the PZEM
+ * Pin 5 Rx (Connects to the Tx pin on the PZEM)
+ * Pin 4 Tx (Connects to the Rx pin on the PZEM)
+*/
+
+
+
+WiFiManager wifiManager;
+#include <supla/network/esp_wifi.h>
+Supla::ESPWifi wifi("", "");  //------ Do not change----wifimanager takes care------
+
+/*
+class MyDigitalRead : public Supla::Io {
+  public:
+    int customDigitalRead(int channelNumber, uint8_t pin) {
+      if (channelNumber == 0) { // ---------------------------- we select the channel we want ------------------
+        Serial.print("customRead: ");Serial.print(channelNumber);
+        return x; // ------------------------- x = the value we want --- for example  return sensors.getTempCByIndex(0);
+      } else {
+        return ::digitalRead(pin);  // ------------------------------ so that the other channels work normally
+      }
+    }
+}instanceMyDigitalRead;
+
+class MyDigitalWrite : public Supla::Io {
+  public:
+    void customDigitalWrite(int channelNumber, uint8_t pin, uint8_t val) {
+      if (channelNumber == 0) {   // ---------------------------- we select the channel we want ------------------
+        Serial.print("customWrite: ");Serial.print(val);     
+        return;
+      }else {
+        return ::digitalWrite(pin,val);   // ------------------------------ so that the other channels work normally
+     // }
+   }
+}instanceMyDigitalWrite; 
+*/
+void saveConfigCallback () {          
+  Serial.println("Should save config");
+  shouldSaveConfig = true;
+}
+void ondemandwifiCallback () {
+   
+   WiFiManagerParameter custom_Supla_server("server", "supla server", Supla_server, 81,"required");
+   WiFiManagerParameter custom_Email("email", "Email", Email, 81,"required");
+   WiFiManagerParameter custom_Wys("wys", "Wysokość npm [ m ]", Wys, 51,"required");
+   WiFiManagerParameter custom_Supla_name("name", "Supla Device Name", Supla_name, 51,"required");
+   WiFiManagerParameter custom_Supla_status("status", "Supla Last State", Supla_status, 51,"readonly");
+   
+   wifiManager.setBreakAfterConfig(true);
+   wifiManager.setSaveConfigCallback(saveConfigCallback);
+   //wifiManager.setSaveParamsCallback(saveParamCallback);  // ---part of custom Field ( buttons )--
+  
+   wifiManager.addParameter(&custom_Supla_server);
+   wifiManager.addParameter(&custom_Email);
+    wifiManager.addParameter(&custom_Wys);
+   wifiManager.addParameter(&custom_Supla_name);
+  // wifiManager.addParameter(&custom_field);  // ---part of custom Field ( buttons )--
+   wifiManager.addParameter(&custom_Supla_status);
+
+   wifiManager.setCustomHeadElement("<style>html{ background-color: #01DF3A;}</style><div class='s'><svg version='1.1' id='l' x='0' y='0' viewBox='0 0 200 200' xml:space='preserve'><path d='M59.3,2.5c18.1,0.6,31.8,8,40.2,23.5c3.1,5.7,4.3,11.9,4.1,18.3c-0.1,3.6-0.7,7.1-1.9,10.6c-0.2,0.7-0.1,1.1,0.6,1.5c12.8,7.7,25.5,15.4,38.3,23c2.9,1.7,5.8,3.4,8.7,5.3c1,0.6,1.6,0.6,2.5-0.1c4.5-3.6,9.8-5.3,15.7-5.4c12.5-0.1,22.9,7.9,25.2,19c1.9,9.2-2.9,19.2-11.8,23.9c-8.4,4.5-16.9,4.5-25.5,0.2c-0.7-0.3-1-0.2-1.5,0.3c-4.8,4.9-9.7,9.8-14.5,14.6c-5.3,5.3-10.6,10.7-15.9,16c-1.8,1.8-3.6,3.7-5.4,5.4c-0.7,0.6-0.6,1,0,1.6c3.6,3.4,5.8,7.5,6.2,12.2c0.7,7.7-2.2,14-8.8,18.5c-12.3,8.6-30.3,3.5-35-10.4c-2.8-8.4,0.6-17.7,8.6-22.8c0.9-0.6,1.1-1,0.8-2c-2-6.2-4.4-12.4-6.6-18.6c-6.3-17.6-12.7-35.1-19-52.7c-0.2-0.7-0.5-1-1.4-0.9c-12.5,0.7-23.6-2.6-33-10.4c-8-6.6-12.9-15-14.2-25c-1.5-11.5,1.7-21.9,9.6-30.7C32.5,8.9,42.2,4.2,53.7,2.7c0.7-0.1,1.5-0.2,2.2-0.2C57,2.4,58.2,2.5,59.3,2.5z M76.5,81c0,0.1,0.1,0.3,0.1,0.6c1.6,6.3,3.2,12.6,4.7,18.9c4.5,17.7,8.9,35.5,13.3,53.2c0.2,0.9,0.6,1.1,1.6,0.9c5.4-1.2,10.7-0.8,15.7,1.6c0.8,0.4,1.2,0.3,1.7-0.4c11.2-12.9,22.5-25.7,33.4-38.7c0.5-0.6,0.4-1,0-1.6c-5.6-7.9-6.1-16.1-1.3-24.5c0.5-0.8,0.3-1.1-0.5-1.6c-9.1-4.7-18.1-9.3-27.2-14c-6.8-3.5-13.5-7-20.3-10.5c-0.7-0.4-1.1-0.3-1.6,0.4c-1.3,1.8-2.7,3.5-4.3,5.1c-4.2,4.2-9.1,7.4-14.7,9.7C76.9,80.3,76.4,80.3,76.5,81z M89,42.6c0.1-2.5-0.4-5.4-1.5-8.1C83,23.1,74.2,16.9,61.7,15.8c-10-0.9-18.6,2.4-25.3,9.7c-8.4,9-9.3,22.4-2.2,32.4c6.8,9.6,19.1,14.2,31.4,11.9C79.2,67.1,89,55.9,89,42.6z M102.1,188.6c0.6,0.1,1.5-0.1,2.4-0.2c9.5-1.4,15.3-10.9,11.6-19.2c-2.6-5.9-9.4-9.6-16.8-8.6c-8.3,1.2-14.1,8.9-12.4,16.6C88.2,183.9,94.4,188.6,102.1,188.6z M167.7,88.5c-1,0-2.1,0.1-3.1,0.3c-9,1.7-14.2,10.6-10.8,18.6c2.9,6.8,11.4,10.3,19,7.8c7.1-2.3,11.1-9.1,9.6-15.9C180.9,93,174.8,88.5,167.7,88.5z'/></svg>");
+   wifiManager.setMinimumSignalQuality(8);
+   //wifiManager.setShowStaticFields(true); // force show static ip fields
+   //wifiManager.setShowDnsFields(true);    // force show dns field always
+   wifiManager.setConfigPortalTimeout(180);
+
+   if (!wifiManager.startConfigPortal("Supla_WiFi")) { Serial.println("Not connected to WiFi but continuing anyway.");} else { Serial.println("connected...yeey :)");}                
+    strcpy(Supla_server, custom_Supla_server.getValue());
+    strcpy(Email, custom_Email.getValue());
+     strcpy(Wys, custom_Wys.getValue());
+     strcpy(Supla_name, custom_Supla_name.getValue());
+    //strcpy(Supla_name, custom_Supla_name.getValue());   // ---part of custom Field ( buttons )--
+    WiFi.softAPdisconnect(true);   //  close AP
+}
+/*
+String getParam(String name){   // ---part of custom Field ( buttons )--
+  String value;
+  if(wifiManager.server->hasArg(name)) {
+    value = wifiManager.server->arg(name);
+      if (value == "1"){ 
+        
+         }
+  }
+  return value;
+}
+
+void saveParamCallback(){   // ---part of custom Field ( buttons )--
+  Serial.println("[CALLBACK] saveParamCallback fired");
+  Serial.println("PARAM customfieldid = " + getParam("customfieldid"));
+}
+*/
+void status_func(int status, const char *msg) {    //    ------------------------ Status --------------------------
+  if (status != 10){
+     strcpy(Supla_status, msg); 
+  }              
+}
+// DHT22 Sensor read implementation
+void get_temperature_and_humidity(int channelNumber, double *temp, double *humidity) {
+
+     *temp = bme.readTemperature();
+    Serial.print("Temperature = ");
+    Serial.print(bme.readTemperature());
+    Serial.println(" *C");
+    
+    *humidity = bme.readHumidity();
+    Serial.print("Humidity = ");
+    Serial.print(bme.readHumidity());
+    Serial.println(" %");
+    if ( isnan(*temp) || isnan(*humidity) ) {
+      *temp = -275;
+      *humidity = -1;
+    }
+  
+}
+
+double get_pressure(int channelNumber, double pressure) {
+ //t = bme.readPressure() / pow(2.718281828, -(Wys / ((273.15 + bme.readTemperature()) * 29.263))) / 100.0  
+    
+    pressure = bme.readPressure() / pow(2.718281828, -(npm / ((273.15 + bme.readTemperature()) * 29.263))) / 100.0;  
+      Serial.print("Pressure = ");
+    Serial.print(bme.readPressure()/100);
+    Serial.println(" hPa");
+   return  pressure;   
+  }
+
+double get_distance(int channelNumber, double t) {
+
+   double lux = LightSensor.GetLightIntensity();     //BH1750
+    t = abs(lux);
+    Serial.print("Natezenie swiatla = ");
+    Serial.print(LightSensor.GetLightIntensity());
+    delay(1250);
+    return t;
+  }
+
+void setup() {
+  wifi_set_sleep_type(NONE_SLEEP_T);
+  Serial.begin(115200);
+
+  LightSensor.begin(); 
+  Wire.begin(4,5);
+  bme.begin(0x76);
+  
+  if (WiFi.SSID()==""){ initialConfig = true;} 
+
+  if (SPIFFS.begin()) {  // ------------------------- wificonfig read -----------------
+    Serial.println("mounted file system");
+    if (SPIFFS.exists("/config.json")) {
+      Serial.println("reading config file");
+      File configFile = SPIFFS.open("/config.json", "r");
+      if (configFile) {
+        Serial.println("opened config file");
+        size_t size = configFile.size();
+        std::unique_ptr<char[]> buf(new char[size]);
+        configFile.readBytes(buf.get(), size);
+        DynamicJsonBuffer jsonBuffer;         
+        JsonObject& json = jsonBuffer.parseObject(buf.get());
+        json.printTo(Serial);   //print config data to serial on startup
+        if (json.success()) {Serial.println("\nparsed json");         
+          strcpy(Supla_server, json["Supla_server"]);
+          strcpy(Email, json["Email"]);
+          strcpy(Wys, json["Wys"]);
+          strcpy(Supla_name, json["Supla_name"]);  
+          npm = atoi(Wys);  // ---------------------------------- konwersja char to int ----------------------------------------------------       
+        } else {
+          Serial.println("failed to load json config");
+          initialConfig = true;
+        }
+      }
+    }
+  } else {
+    Serial.println("failed to mount FS");
+  }
+
+  SuplaDevice.setTemperatureHumidityCallback(&get_temperature_and_humidity);
+ 
+   uint8_t mac[WL_MAC_ADDR_LENGTH];
+   WiFi.macAddress(mac);
+   char GUID[SUPLA_GUID_SIZE] = {mac[WL_MAC_ADDR_LENGTH - 6], mac[WL_MAC_ADDR_LENGTH - 5], mac[WL_MAC_ADDR_LENGTH - 4], mac[WL_MAC_ADDR_LENGTH - 3],                                
+                                 mac[WL_MAC_ADDR_LENGTH - 2], mac[WL_MAC_ADDR_LENGTH - 1], mac[WL_MAC_ADDR_LENGTH - 1], mac[WL_MAC_ADDR_LENGTH - 2], 
+                                 mac[WL_MAC_ADDR_LENGTH - 3], mac[WL_MAC_ADDR_LENGTH - 4], mac[WL_MAC_ADDR_LENGTH - 5], mac[WL_MAC_ADDR_LENGTH - 6]};
+   char AUTHKEY[SUPLA_AUTHKEY_SIZE] = {mac[WL_MAC_ADDR_LENGTH - 1], mac[WL_MAC_ADDR_LENGTH - 2],mac[WL_MAC_ADDR_LENGTH - 3], mac[WL_MAC_ADDR_LENGTH - 4],
+                                       mac[WL_MAC_ADDR_LENGTH - 5], mac[WL_MAC_ADDR_LENGTH - 6],mac[WL_MAC_ADDR_LENGTH - 6], mac[WL_MAC_ADDR_LENGTH - 5],
+                                       mac[WL_MAC_ADDR_LENGTH - 4], mac[WL_MAC_ADDR_LENGTH - 3],mac[WL_MAC_ADDR_LENGTH - 2], mac[WL_MAC_ADDR_LENGTH - 1]};
+                             
+ 
+    SuplaDevice.addDHT22();  
+    SuplaDevice.addPressureSensor();  // cisnienie
+    SuplaDevice.addDistanceSensor();
+    SuplaDevice.setName(Supla_name);
+    SuplaDevice.setStatusFuncImpl(&status_func);
+  
+  SuplaDevice.begin(GUID,              // Global Unique Identifier 
+                    Supla_server,  // SUPLA server address
+                    Email,   // Email address used to login to Supla Cloud
+                    AUTHKEY);          // Authorization key
+                      
+}
+
+void loop() {
+  if (initialConfig == true){ondemandwifiCallback();}
+
+  int C_W_read = digitalRead(wificonfig_pin);{  // ---------------------let's read the status of the Gpio to start wificonfig ---------------------
+   if (C_W_read != last_C_W_state) {  time_last_C_W_change = millis();}      
+    if ((millis() - time_last_C_W_change) > C_W_delay) {     
+     if (C_W_read != C_W_state) {     
+       C_W_state = C_W_read;       
+       if (C_W_state == LOW) {
+        ondemandwifiCallback () ;} } }         
+     last_C_W_state = C_W_read;            
+   }
+      
+  if (shouldSaveConfig == true) { // ------------------------ wificonfig save --------------
+    Serial.println("saving config");
+    DynamicJsonBuffer jsonBuffer;
+    JsonObject& json = jsonBuffer.createObject();
+    json["Supla_server"] = Supla_server;
+    json["Email"] = Email;
+    json["Wys"] = Wys;
+    json["Supla_name"] = Supla_name;
+    File configFile = SPIFFS.open("/config.json", "w");
+    if (!configFile) {Serial.println("failed to open config file for writing"); }   
+    json.prettyPrintTo(Serial);
+    json.printTo(configFile);
+    configFile.close();
+    Serial.println("config saved");    
+    shouldSaveConfig = false;
+    initialConfig = false; 
+    WiFi.mode(WIFI_STA);   
+    delay(5000);
+    ESP.restart(); 
+  }
+  
+  SuplaDevice.iterate();
+}
